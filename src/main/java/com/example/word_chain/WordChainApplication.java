a353283b58e69aaec7abf9edfdf35abf5fd838b8
HorunFujimoto <horun.fujimoto@weserve.co.jp>
2025-02-05 14:56:41 +0900

    SpringBootプロジェクト作成

---
+++ src/main/java/com/example/word_chain/WordChainApplication.java
@@ -0,0 +1,13 @@
+package com.example.word_chain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordChainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordChainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/word_chain/WordChainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.word_chain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordChainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
